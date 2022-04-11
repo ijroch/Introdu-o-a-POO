5d3890989135ed293c884e05dbcb8059584db7be
ijroch <50756947+ijroch@users.noreply.github.com>
2022-04-11 14:55:19 -0300

    Add files via upload

---
+++ Exercicio1/Exercicio1/src/main/java/Agenda.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**Faça um programa de agenda telefônica, com as classes Agenda e Contato
+ *
+ * @author isinhazinha
+ */
+public class Agenda {
+    ArrayList<Contato> arrayDeProdutos= new ArrayList<>();   
+}
